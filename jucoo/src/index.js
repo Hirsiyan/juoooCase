d605aee39fa98b8b8ac854084278a5a74d235644
Hirsiyan <13512879051@163.com>
2019-09-17 14:18:33 +0800

    xixi

--- jucoo/src/index.js
+++ jucoo/src/index.js
@@ -7,8 +7,9 @@ import 'lib-flexible';
 import {Provider} from "react-redux"
 import store from "./store"
 import MyMoney from "./views/MyMoney";
-import Myintegral from "./views/Myintegral"; ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
-//ReactDOM.render(<MyMoney/>, document.getElementById('root'));
+ import Myintegral from "./views/Myintegral";
+ ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+ // ReactDOM.render(<MyMoney/>, document.getElementById('root'));
 //ReactDOM.render(<Myintegral/>, document.getElementById('root'));
 //
 // If you want your app to work offline and load faster, you can change
--- jucoo/src/views/MyJucco.js
+++ jucoo/src/views/MyJucco.js
@@ -12,9 +12,42 @@ class MyJucco extends React.Component{
                                     </i>
                                 </a>
                                 <div className={"mine-info__main"}>
-                                    <div className={"mine-info__main__head"}></div>
-                                    <div className={"mine-info__main__label"}></div>
-                                    <div className={"mine-info__main__cnt"}></div>
+                                    <div className={"mine-info__main__head"}>
+                                        <div class={"mine-info_head_pic"}>
+                                            <img src="https://image.juooo.com/group1/M00/03/6F/rAoKNV0XF2uABEtSAAANUrP00o0602.png" alt=""/>
+                                        </div>
+                                        <span className={"mine-info_userName"}>账户</span>
+                                        <span className={"mine-info_ID"}>ID:123456</span>
+                                    </div>
+                                    <div className={"mine-info_main_label"}>
+                                        <p className={"mine-info_main_vip"}>普通会员</p>
+                                    </div>
+                                    <div className={"mine-info_main_cnt"}>
+                                        <a href="" className={"mine-info_main_cnt_item"}>
+                                            <p className={"mine-info_main_cnt_item_num"}>
+                                                0
+                                            </p>
+                                            <p className={"mine-info_main_cnt_item_name"}>
+                                                账户余额
+                                            </p>
+                                        </a>
+                                        <a href="" className={"mine-info_main_cnt_item"}>
+                                            <p className={"mine-info_main_cnt_item_num"}>
+                                                6
+                                            </p>
+                                            <p className={"mine-info_main_cnt_item_name"}>
+                                                积分
+                                            </p>
+                                        </a>
+                                        <a href="" className={"mine-info_main_cnt_item"}>
+                                            <p className={"mine-info_main_cnt_item_num"}>0</p>
+                                            <p className={"mine-info_main_cnt_item_name"}>优惠券</p>
+                                        </a>
+                                        <a href="" className={"mine-info_main_cnt_item"}>
+                                            <p className={"mine-info_main_cnt_item_num"}>立即开通</p>
+                                            <p className={"mine-info_main_cnt_item_name"}>橙PLUS卡</p>
+                                        </a>
+                                    </div>
                                 </div>
 
                             </div>
