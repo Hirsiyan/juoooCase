7c1f68e81ac9e10667b8776e0600772f64334891
Aaronlike <17624031995@163.com>
2019-09-17 22:51:45 +0800

    张智博第一次上传

--- jucoo/src/index.js
+++ jucoo/src/index.js
@@ -8,4 +8,8 @@ import {Provider} from "react-redux";
 import store from "./store";
 import "antd/dist/antd.css";
 import "./assets/css/reset.css";
-ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+import Plus from "./views/Plus"
+import Integral from "./views/Integral";
+// ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+// ReactDOM.render(<Plus />, document.getElementById('root'));
+ReactDOM.render(<Integral />, document.getElementById('root'));
---
+++ jucoo/src/views/Integral.js
@@ -0,0 +1,25 @@
+import React from "react"
+import "../assets/css/Integral.css"
+import "../assets/fonts/iconfont.css"
+class Integral extends React.Component{
+    render() {
+        return(
+            <div className="views">
+                <div className="nav1">
+                    <div className="nav-inner">
+                        <div className="left">
+                            <span className="icon-menu-left iconfont">&#xe603;</span>
+                        </div>
+                        <div className="center">积分商城</div>
+                        <div className="right">
+                            <span className="icon-menu-right iconfont">&#xe600;</span>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+        )
+    }
+}
+export default Integral;
