4e8d2b7f504e48d16f4aafdf9fa3370864756c41
Hirsiyan <13512879051@163.com>
2019-09-17 23:15:01 +0800

    闫晓强第一次上传

--- jucoo/src/index.js
+++ jucoo/src/index.js
@@ -7,11 +7,12 @@ import 'lib-flexible';
 import {Provider} from "react-redux"
 import store from "./store"
 import "./assets/css/reset.css"
-import MyMoney from "./views/MyMoney";
- import Myintegral from "./views/Myintegral";
- // ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
- ReactDOM.render(<MyMoney/>, document.getElementById('root'));
-//ReactDOM.render(<Myintegral/>, document.getElementById('root'));
+
+// import MyMoney from "./views/MyMoney";
+// import Myintegral from "./views/Myintegral";
+ ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+ // ReactDOM.render(<MyMoney/>, document.getElementById('root'));
+// ReactDOM.render(<Myintegral/>, document.getElementById('root'));
 //
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
